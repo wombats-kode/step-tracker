4e0b5311a9852a0d7f1111b15a4b74c8ec0e98a2
wombats-kode <wombat1913@gmail.com>
2024-05-20 09:02:57 +0100

    created xcode project

---
+++ Step Tracker/Step_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Step_TrackerApp.swift
+//  Step Tracker
+//
+//  Created by Richard Harris on 20/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct Step_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
