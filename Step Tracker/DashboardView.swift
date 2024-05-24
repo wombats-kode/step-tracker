6e98f71931b1b638d797d25e3f1cc0213f8899bb
wombats-kode <wombat1913@gmail.com>
2024-05-24 10:40:26 +0100

    added healthkit priming Appstorage

--- Step Tracker/DashboardView.swift
+++ Step Tracker/DashboardView.swift
@@ -24,6 +24,8 @@ enum HealthMetricContext: CaseIterable, Identifiable {
 
 struct DashboardView: View {
     
+    @AppStorage("hasSeenPermissionPriming") private var hasSeenPermissionPriming = false
+    @State private var isShowingPermissionPrimingSheet = false
     @State private var selectedStat: HealthMetricContext = .steps
     var isSteps: Bool { selectedStat == .steps}
     
@@ -87,10 +89,18 @@ struct DashboardView: View {
                     }
                 }
                 .padding()
+                .onAppear {
+                    isShowingPermissionPrimingSheet = !hasSeenPermissionPriming
+                }
                 .navigationTitle("Dashboard")
                 .navigationDestination(for: HealthMetricContext.self) { metric in
                     HealthDataListView(metric: metric)
                 }
+                .sheet(isPresented: $isShowingPermissionPrimingSheet, onDismiss: {
+                    // fetch health data
+                }, content: {
+                    HealthKitPermissionPrimingView(hasSeen: $isShowingPermissionPrimingSheet)
+                })
             }
             .tint(isSteps ? .pink : .indigo)
     }
@@ -98,4 +108,5 @@ struct DashboardView: View {
 
 #Preview {
     DashboardView()
+        .environment(HealthKitManager())
 }
